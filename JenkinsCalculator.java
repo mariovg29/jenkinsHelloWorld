3d79adfefb314fa23ffa758b1fdde6dbd849d03e
Mario Valadez <mario.valadez@solera.com>
2022-11-10 15:05:11 -0600

    Add new file

---
+++ JenkinsCalculator.java
@@ -0,0 +1,16 @@
+package com.mario.trainning.jenkins;
+
+public class JenkinsCalculator {
+	
+	public int addTwoNumbers(int numberOne, int numberTwo) {
+		int res = numberOne + numberTwo ;
+		return res;
+		
+	}
+	public int substracTwoNumbers(int numberOne, int numberTwo) {
+		int res = numberOne - numberTwo ;
+		return res;
+		
+	}
+
+}
